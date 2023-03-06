27642752c6406ca5153417b762963d17ef2b3cea
dunnie99 <oluwadunnirachael@gmail.com>
2023-03-06 12:25:39 +0100

    update script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -28,46 +28,50 @@ async function main() {
   const csvFile = fs.readFileSync("claimers.csv", "utf-8");
   const claimersCsv = csvFile.split("\n").splice(1);
   console.log(`your csv is ${claimersCsv}`);
-  const headers = ['Address', 'Amount'];
-  
+  const headers = ['address', 'amount'];
+
   const jsonto = "claimers.json";
 
   const output: any[] =[];
   parse(csvFile, {
     delimiter: ',',
     columns: headers,
-  }, (error, result
-    : data[]
-    ) => {
+  }, (error, result: data[] ) => {
       if (error){
         console.error(error);
       }
       const jsonObj = JSON.stringify(result);
-      fs.writeFileSync(jsonto, jsonObj);  
+      fs.writeFileSync(jsonto, jsonObj); 
+      result.map((e)=>{
+        output.push([e.Address, e.Amount])
+      }) 
   });
-
-
 
   const merkleFile = fs.readFileSync(jsonto,"utf-8");
   console.log(`merkleFile is ${merkleFile}`);
 
 
-  const JSONList = JSON.parse(merkleFile);
+  const tree = StandardMerkleTree.of(output, ['address', 'uint256']);
+  console.log(tree.root);
+  
 
-  JSONList.forEach((item: any )=> {
-    console.log(item.address, item.amount);
+
+  
+
+  // JSONList.forEach((item: any )=> {
+  //   console.log(item.address, item.amount);
     
-  });
+  // });
 
-  const byteData = ethers.utils.defaultAbiCoder.encode(
-    ['address', 'uint256'],
-    [JSONList.address, JSONList.amount]
-  )
-  console.log(`byteData is ${byteData}`);
+  // const byteData = ethers.utils.defaultAbiCoder.encode(
+  //   ['address', 'uint256'],
+  //   [JSONList.address, JSONList.amount]
+  // )
+  // console.log(`byteData is ${byteData}`);
   
   
-  const leaves = JSONList.map((item: any) => keccak256(JSON.stringify(item)));
-  const CSVmerkleTree = new MerkleTree(leaves,keccak256,{sort:true});
+  // const leaves = JSONList.map((item: any) => keccak256(JSON.stringify(item)));
+  // const CSVmerkleTree = new MerkleTree(leaves,keccak256,{sort:true});
 
   // console.log("generated merkle root is", CSVmerkleTree.getRoot().toString("hex"));
 
